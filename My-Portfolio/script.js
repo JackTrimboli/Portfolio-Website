1681563fa217f44a72c60887113e34daa35bc912
JACK TRIMBOLI <Jack.Trimboli@yahoo.com>
2021-01-01 21:44:25 -0500

    first commit

---
+++ My-Portfolio/script.js
@@ -0,0 +1,25 @@
+const hamburger = document.getElementById('hamburger')
+const navUL = document.getElementById('nav-ul')
+const boxes = document.querySelectorAll('.projects-box')
+
+hamburger.addEventListener('click', () =>{
+    navUL.classList.toggle('show')
+});
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes(){
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
